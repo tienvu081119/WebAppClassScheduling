34709887b586b3b7831e0cede523597fcabf66c5
tienvu081119 <tienvu081119@gmail.com>
2021-10-17 14:48:29 +0700

    add TimeslotUI

---
+++ WebApp/Controllers/TimeslotController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class TimeslotController : Controller
+    {
+        TimeslotRepository repository;
+
+        public TimeslotController(CSContext context)
+        {
+            repository = new TimeslotRepository(context);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            repository.Delete(id);
+            return Redirect("/timeslot");
+        }
+
+        public IActionResult Index()
+        {
+            return View(repository.GetTimeslots());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Timeslot obj)
+        {
+            repository.Add(obj);
+            return Redirect("/timeslot");
+        }
+    }
+}
---
+++ WebApp/Models/TimeslotRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class TimeslotRepository
+    {
+        CSContext context;
+        public TimeslotRepository(CSContext context)
+        {
+            this.context = context;
+        }
+
+        public int Delete(int id)
+        {
+            context.Timeslots.Remove(new Timeslot { Id = id });
+            return context.SaveChanges();
+        }
+
+        public List<Timeslot> GetTimeslots()
+        {
+            return context.Timeslots.ToList();
+        }
+
+        public int Add(Timeslot obj)
+        {
+            context.Timeslots.Add(obj);
+            return context.SaveChanges();
+        }
+
+    }
+}
